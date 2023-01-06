6633b6dbd6d3c62fcac044ee7b23bd56af392b88
healthyteen <118077323+healthyteen@users.noreply.github.com>
2023-01-06 13:09:07 -0800

    Completed Branding And Functionality

---
+++ 2048/Sources/Audio/HapticManager.swift
@@ -0,0 +1,21 @@
+//
+//  HapticManager.swift
+//  2048
+//
+//  Created by Skyler Szijjarto on 1/6/23.
+//
+
+import SwiftUI
+
+class HapticManager {
+    static let instance = HapticManager() // Singleton
+    
+    func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+    func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
+    }
+}
