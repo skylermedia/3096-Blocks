26935a97c786af591be6a0ef3990833f67f3d38c
healthyteen <118077323+healthyteen@users.noreply.github.com>
2023-01-11 20:38:59 -0800

    Made Image Circular

--- 2048/Sources/Views/About/AboutView.swift
+++ 2048/Sources/Views/About/AboutView.swift
@@ -44,8 +44,8 @@ struct AboutView: View {
         VStack(spacing: 16) {
             Image("Icon")
                 .resizable()
-                .cornerRadius(proxy.size.width / 50)
                 .aspectRatio(contentMode: .fit)
+                .clipShape(Circle())
             if #available(iOS 16.0, *) {
                 Link(about[PlistConfigurationKeyPath.about.rawValue]?[PlistConfigurationKeyPath.linkDescription.rawValue] ?? "[Missing Information]",
                      destination: URL(string: about[PlistConfigurationKeyPath.about.rawValue]?[PlistConfigurationKeyPath.linkUrl.rawValue] ?? "")!)
