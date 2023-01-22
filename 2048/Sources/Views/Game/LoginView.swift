a7c5a595b6a9ac36b8672db98b755d72e4c5686f
healthyteen <118077323+healthyteen@users.noreply.github.com>
2023-01-22 09:11:57 -0800

    Optimized LoginView

--- 2048/Sources/Views/Game/LoginView.swift
+++ 2048/Sources/Views/Game/LoginView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct LoginView: View {
+    @Environment(\.colorScheme) private var colorScheme: ColorScheme
+
     @State private var userName: String = ""
     @State var boardSize: Int
     @State private var isLoggedIn: Bool = false
+    
+    private var textColor: Color {
+        colorScheme == .dark ? Color(red:0.90, green:0.90, blue:0.90, opacity:1.00) : Color(red:0.10, green:0.10, blue:0.10, opacity:1.00)
+    }
+    
+    private var usernameTextColor: Color {
+        colorScheme == .light ? Color(red:0.90, green:0.90, blue:0.90, opacity:1.00) : Color(red:0.10, green:0.10, blue:0.10, opacity:1.00)
+    }
+    
+    private var backgroundColor: Color {
+        colorScheme == .dark ? Color(red:0.90, green:0.90, blue:0.90, opacity:1.00) : Color(red:0.10, green:0.10, blue:0.10, opacity:1.00)
+    }
     
     var body: some View {
         NavigationView {
@@ -19,10 +33,16 @@ struct LoginView: View {
                 Text("Enter a Username Below")
                     .bold()
                     .font(.title)
-                TextField("", text: $userName)
-                    .padding()
-                    .background(.primary)
-                    .cornerRadius(25)
+                    .foregroundColor(textColor)
+                HStack {
+                    Spacer()
+                    TextField("", text: $userName)
+                        .padding()
+                        .foregroundColor(usernameTextColor)
+                        .background(backgroundColor)
+                        .cornerRadius(25)
+                    Spacer()
+                }
                 Button("Submit") {
                     setUserName()
                 }
@@ -38,8 +58,8 @@ struct LoginView: View {
                 }
                 Spacer()
             }
-            .navigationBarBackButtonHidden(isLoggedIn)
         }
+        .navigationBarBackButtonHidden(isLoggedIn)
     }
     
     func setUserName() {
