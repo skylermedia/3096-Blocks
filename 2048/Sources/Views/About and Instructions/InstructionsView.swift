11c410dc2408ac2cbfc65dd8d2cf68051c1a8e8f
healthyteen <github@healthyteen.org>
2023-03-02 18:45:08 -0800

    Update InstructionsView.swift

--- 2048/Sources/Views/About and Instructions/InstructionsView.swift
+++ 2048/Sources/Views/About and Instructions/InstructionsView.swift
@@ -25,33 +25,26 @@ struct InstructionsView: View {
     var body: some View {
         GeometryReader { proxy in
             iconHeader(padding: UIDevice.current.userInterfaceIdiom == .pad ? 64 : 32, proxy: proxy)
-            
-            HStack {
-                Text(about[PlistConfigurationKeyPath.about.rawValue]?[PlistConfigurationKeyPath.copyright.rawValue] ?? "Missing Information")
-                    .font(Font.system(.callout, design: .monospaced).weight(.bold))
-                    .foregroundColor(.secondary)
-            }
-            .position(x: proxy.frame(in: .local).midX, y: proxy.frame(in: .local).maxY)
-            
         }
-        .padding(.bottom, 24)
-        .padding([.leading, .trailing], 48)
+        .padding([.leading, .trailing], 24)
     }
 
     // MARK: - Private Methods
 
     private func iconHeader(padding spacing: CGFloat, proxy: GeometryProxy) -> some View {
         VStack(spacing: 8) {
-            Image("Icon")
-                .resizable()
-                .cornerRadius(25)
-                .aspectRatio(contentMode: .fit)
-            
-            Text("")
-                .multilineTextAlignment(.center)
-                .fontWeight(.bold)
-                .font(Font.system(.callout, design: .monospaced).weight(.bold))
-                .foregroundColor(.primary)
+            if #available(iOS 16.0, *) {
+                Text("Instructions Will be Here Soon")
+                    .multilineTextAlignment(.center)
+                    .fontWeight(.bold)
+                    .font(Font.system(.callout, design: .monospaced).weight(.bold))
+                    .foregroundColor(.primary)
+            } else {
+                Text("Instructions Will be Here Soon")
+                    .multilineTextAlignment(.center)
+                    .font(Font.system(.callout, design: .monospaced).weight(.bold))
+                    .foregroundColor(.primary)
+            }
         }
         .padding(.all, spacing)
         .center(in: .local, with: proxy)
